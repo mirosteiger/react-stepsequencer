73c0e1875ba5bc30bde8bc1e4900cfa10d451906
Mironimo2 <miro.steiger@outlook.de>
2022-04-06 14:04:23 +0200

    Dark and Light Theme

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -2,21 +2,81 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   display: flex;
-  width: 100vw;
-  height: 100vh;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  background-color: #333;
+  width: 100vw;
+  height: 100vh;
+  background-color: ${(props) => props.theme.background};
+`;
+
+export const Header = styled.header`
+  width: 100vw;
+  height: 10vh;
+  justify-content: flex-end;
+  margin-bottom: 1rem;
+`;
+
+export const SequencerWrapper = styled.div`
+  width: 100vw;
+  justify-content: center;
+  align-content: center;
+  flex-basis: auto;
+  flex-grow: 1;
 `;
 
 export const SequencerCase = styled.div`
-  width: 100em;
-  height: 50em;
-  background-color: #1e1e1e;
-  border-radius: 14px;
-  box-shadow: 0 0 1em #111; ;
+  width: 80%;
+  height: 80%;
+  margin: auto;
+  background-color: ${(props) => props.theme.material};
+  border-radius: 10px;
+  box-shadow: 0 0 0.8em ${(props) => props.theme.shadow}50;
+  color: ${(props) => props.theme.text};
 `;
 
-export const TransportBar = styled.div`
-  border-bottom: solid 1px antiquewhite;
+export const TransportBarWrapper = styled.div`
+  height: 14%;
+  border-bottom: solid 1px;
 `;
+
+export const TransportItem = styled.div`
+  flex-grow: 1;
+`;
+
+export const FlexContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  align-items: stretch;
+  flex-direction: row;
+  height: 100%;
+`;
+
+export const CustomToggle = styled.button`
+  padding: 1rem;
+  border: none;
+  outline: none;
+  font-size: 2rem;
+  cursor: pointer;
+  transition: 0.2s, all ease-in-out;
+  background: none;
+  color: ${(props) => props.theme.primary};
+  position: absolute;
+  top: 0;
+  right: 0;
+
+  &:hover {
+    transition: 0.2s all ease-in-out;
+  }
+`;
+
+export const ToggleIcon = styled.span`
+  font-size: 100%;
+`;
+export const Button = styled.button`
+  border-radius: 50%;
+  padding: 10px;
+`;
+
+export const Select = styled.select``;
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,5 +1,19 @@
-const theme = {
-    colors: {
-        background: "#333",
-    }
-}
+export const darkTheme = {
+  background: "#1e1e1e",
+  material: "#333333",
+  text: "#E5E3C9",
+  primary: "#064663",
+  secondary: "#ECB365",
+  selected: "",
+  shadow: "#111111",
+};
+
+export const lightTheme = {
+  background: "#B4CFB0",
+  material: "#E5E3C9",
+  text: "#041C32",
+  primary: "#94B49F",
+  secondary: "#789395",
+  selected: "",
+  shadow: "#111111",
+};
